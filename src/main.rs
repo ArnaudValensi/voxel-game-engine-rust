68004cd019f2b15cd9230140124da8484d3e7155
Arnaud Valensi <arnaud.valensi@gmail.com>
2018-11-10 16:56:52 +0100

    Fix lint

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ extern crate glutin;
 
 use gfx::traits::FactoryExt;
 use gfx::Device;
-use glutin::{GlContext, Event, KeyboardInput, VirtualKeyCode, WindowEvent};
+use glutin::{Event, GlContext, KeyboardInput, VirtualKeyCode, WindowEvent};
 
 pub type ColorFormat = gfx::format::Rgba8;
 pub type DepthFormat = gfx::format::DepthStencil;
@@ -25,9 +25,18 @@ gfx_defines!{
 }
 
 const TRIANGLE: [Vertex; 3] = [
-    Vertex { pos: [ -0.5, -0.5 ], color: [1.0, 0.0, 0.0] },
-    Vertex { pos: [  0.5, -0.5 ], color: [0.0, 1.0, 0.0] },
-    Vertex { pos: [  0.0,  0.5 ], color: [0.0, 0.0, 1.0] }
+    Vertex {
+        pos: [-0.5, -0.5],
+        color: [1.0, 0.0, 0.0],
+    },
+    Vertex {
+        pos: [0.5, -0.5],
+        color: [0.0, 1.0, 0.0],
+    },
+    Vertex {
+        pos: [0.0, 0.5],
+        color: [0.0, 0.0, 1.0],
+    },
 ];
 
 const CLEAR_COLOR: [f32; 4] = [0.1, 0.2, 0.3, 1.0];
@@ -40,13 +49,15 @@ pub fn main() {
 
     let (api, version, vs_code, fs_code) = if cfg!(target_os = "emscripten") {
         (
-            glutin::Api::WebGl, (2, 0),
+            glutin::Api::WebGl,
+            (2, 0),
             include_bytes!("shader/triangle_300_es.glslv").to_vec(),
             include_bytes!("shader/triangle_300_es.glslf").to_vec(),
         )
     } else {
         (
-            glutin::Api::OpenGl, (3, 2),
+            glutin::Api::OpenGl,
+            (3, 2),
             include_bytes!("shader/triangle_150_core.glslv").to_vec(),
             include_bytes!("shader/triangle_150_core.glslf").to_vec(),
         )
@@ -59,12 +70,13 @@ pub fn main() {
         gfx_window_glutin::init::<ColorFormat, DepthFormat>(window_config, context, &events_loop);
     let mut encoder = gfx::Encoder::from(factory.create_command_buffer());
 
-    let pso = factory.create_pipeline_simple(&vs_code, &fs_code, pipe::new())
+    let pso = factory
+        .create_pipeline_simple(&vs_code, &fs_code, pipe::new())
         .unwrap();
     let (vertex_buffer, slice) = factory.create_vertex_buffer_with_slice(&TRIANGLE, ());
     let mut data = pipe::Data {
         vbuf: vertex_buffer,
-        out: main_color
+        out: main_color,
     };
 
     // FIXME: On Mac 10.14 (Mojave) we need to resize the window after creation.
@@ -79,18 +91,19 @@ pub fn main() {
         events_loop.poll_events(|event| {
             if let Event::WindowEvent { event, .. } = event {
                 match event {
-                    WindowEvent::CloseRequested |
-                    WindowEvent::KeyboardInput {
-                        input: KeyboardInput {
-                            virtual_keycode: Some(VirtualKeyCode::Escape),
-                            ..
-                        },
+                    WindowEvent::CloseRequested
+                    | WindowEvent::KeyboardInput {
+                        input:
+                            KeyboardInput {
+                                virtual_keycode: Some(VirtualKeyCode::Escape),
+                                ..
+                            },
                         ..
                     } => running = false,
                     WindowEvent::Resized(size) => {
                         window.resize(size.to_physical(window.get_hidpi_factor()));
                         gfx_window_glutin::update_views(&window, &mut data.out, &mut main_depth);
-                    },
+                    }
                     _ => (),
                 }
             }
